f5a6775d39c9241278533e60e9ac8566678ff95d
Ryan Dias <131448240+0RyanSouza0@users.noreply.github.com>
2024-04-11 10:13:42 -0300

    Adicionando um "jogo de advinhação" em Python.

---
+++ jogoAdvinhacao.py
@@ -0,0 +1,18 @@
+
+jogador1 = int(input("Digite um numero de 1 a 10: "))
+while (jogador1 <0 ) or (jogador1>10):
+    print("Número invalido, digite novamente")
+    jogador1 = int(input("Digite um numero de 1 a 10: "))
+
+jogador2 = int
+
+chances = 0
+while jogador1 != jogador2:
+    jogador2 = int(input("Adivinhe o numero digitado pelo jogador 1, o numero é de 1 a 10: "))
+    chances +=1 
+    if(jogador2 == jogador1):
+        print("Voce acertou o numero ")
+        print(f"Você acertou em {chances} vezes"  )
+        
+    
+
